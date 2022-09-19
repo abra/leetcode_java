9718a2c0255a998c718f0fa68487038c1915dc61
Aidar Khabibullin <aydar.kh@gmail.com>
2022-09-19 18:02:54 +0500

    14. Longest Common Prefix (Easy)

--- src/main/java/leetcode/problems/easy/p0014/LongestCommonPrefix.java
+++ src/main/java/leetcode/problems/easy/p0014/LongestCommonPrefix.java
@@ -1,16 +1,46 @@
 package leetcode.problems.easy.p0014;
 
+import java.util.Arrays;
+
 public class LongestCommonPrefix {
     public static void main(String[] args) {
-        String[] strs = {"flower","flow","flight"};
-        System.out.println(new Solution().longestCommonPrefix(strs));
+        String[] strs = {"flower", "flow", "flight"};
+        System.out.println(new Solution().longestCommonPrefix(strs)); // fl
     }
 }
 
+//
+// https://leetcode.com/problems/longest-common-prefix/
+//
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        //
-        return null;
+
+        int len = Arrays.stream(strs)
+                .map(String::length)
+                .min(Integer::compare)
+                .orElse(0);
+
+        String s = "";
+
+        for (int i = 0; i < len; i++) {
+            int counter = 0;
+
+            for (int j = 0, c = 0; j < strs.length && c < strs.length - 1; j++, c++) {
+                if (strs[c].charAt(i) == strs[j + 1].charAt(i)) {
+                    counter++;
+                } else {
+                    break;
+                }
+            }
+
+            if (counter == strs.length - 1) {
+                s += strs[counter].charAt(i);
+            } else {
+                break;
+            }
+        }
+
+        return s.length() == 0 ? "" : s;
     }
 }
 
