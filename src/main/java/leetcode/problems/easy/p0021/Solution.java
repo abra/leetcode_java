22f5ff8957fbca17fb32efaacf45c4877cd3f56a
Aidar Khabibullin <aydar.kh@gmail.com>
2023-04-17 18:03:33 +0500

    Remove some text

--- src/main/java/leetcode/problems/easy/p0021/Solution.java
+++ src/main/java/leetcode/problems/easy/p0021/Solution.java
@@ -2,20 +2,6 @@ package leetcode.problems.easy.p0021;
 
 public class Solution {
 
-//  public ListNode mergeSortedLinkedListsRecursively(ListNode l1, ListNode l2) {
-//
-//    if (l1 == null) return l2;
-//    if (l2 == null) return l1;
-//
-//    if (l1.val < l2.val) {
-//      l1.next = mergeSortedLinkedListsRecursively(l1.next, l2);
-//      return l1;
-//    } else {
-//      l2.next = mergeSortedLinkedListsRecursively(l1, l2.next);
-//      return l2;
-//    }
-//  }
-
   public ListNode mergeSortedLinkedListsIteratively(ListNode l1, ListNode l2) {
 
     if (l1.next == null && l2.next == null) {
@@ -41,35 +27,4 @@ public class Solution {
     return head.next;
   }
 }
-
-/*
-var mergeTwoLists = function(list1, list2) {
-   let newList = new ListNode()
-   let currNew = newList
-   let cur1 = list1
-   let cur2 = list2
-
-   while(cur1 || cur2){
-
-       if(cur1 && !cur2){
-           newList.next = cur1
-           break;
-       }else if(!cur1 && cur2){
-           newList.next = cur2
-           break;
-       }else if( cur1.val <= cur2.val){
-           newList.next = cur1
-           cur1 = cur1.next
-       }else{
-           newList.next = cur2
-           cur2 = cur2.next
-       }
-
-       newList = newList.next
-   }
-
-   return currNew.next
-};
-
- */
 
