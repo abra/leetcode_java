b1d03041b0f760313026d82dc757787aff8af266
Aidar Khabibullin <aydar.kh@gmail.com>
2023-05-15 19:16:11 +0500

    Add solution for problem #3

---
+++ src/main/java/leetcode/problems/medium/p0003/Solution.java
@@ -0,0 +1,33 @@
+package leetcode.problems.medium.p0003;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+
+  public int lengthOfLongestSubstring(String s) {
+    char[] ch = new char[s.length()];
+    s.getChars(0, s.length(), ch, 0);
+    int maxLen = 0;
+
+    List<Character> list = new ArrayList<>();
+
+    for (int i = 0, j = 0; i < ch.length; i++) {
+
+      if (list.contains(ch[i])) {
+        int index = list.lastIndexOf(ch[i]);
+        list.subList(0, index + 1).clear();
+        list.add(ch[i]);
+        i = j++;
+        continue;
+      }
+
+      list.add(ch[i]);
+      maxLen = Math.max(maxLen, list.size());
+      i = j++;
+    }
+
+    return maxLen;
+  }
+
+}
---
+++ src/test/java/leetcode/problems/medium/p0003/SolutionTest.java
@@ -0,0 +1,92 @@
+package leetcode.problems.medium.p0003;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// https://leetcode.com/problems/longest-substring-without-repeating-characters/
+@DisplayName("Leetcode Problem #3. Longest Substring Without Repeating Characters:w")
+class SolutionTest {
+
+  @Test
+  @DisplayName("Input: \"abcabcbb\". Should return: 3")
+  void lengthOfLongestSubstring1() {
+    String input = "abcabcbb";
+    int expected = 3;
+    int result = new Solution().lengthOfLongestSubstring(input);
+
+    assertEquals(expected, result);
+  }
+
+  @Test
+  @DisplayName("Input: \"bbbbb\". Should return: 1")
+  void lengthOfLongestSubstring2() {
+    String input = "bbbbb";
+    int expected = 1;
+    int result = new Solution().lengthOfLongestSubstring(input);
+
+    assertEquals(expected, result);
+  }
+
+  @Test
+  @DisplayName("Input: \"pwwkew\". Should return: 3")
+  void lengthOfLongestSubstring3() {
+    String input = "pwwkew";
+    int expected = 3;
+    int result = new Solution().lengthOfLongestSubstring(input);
+
+    assertEquals(expected, result);
+  }
+
+  @Test
+  @DisplayName("Input: \" \". Should return: 1")
+  void lengthOfLongestSubstring4() {
+    String input = " ";
+    int expected = 1;
+    int result = new Solution().lengthOfLongestSubstring(input);
+
+    assertEquals(expected, result);
+  }
+
+  @Test
+  @DisplayName("Input: \"c\". Should return: 1")
+  void lengthOfLongestSubstring5() {
+    String input = "c";
+    int expected = 1;
+    int result = new Solution().lengthOfLongestSubstring(input);
+
+    assertEquals(expected, result);
+  }
+
+  @Test
+  @DisplayName("Input: \"aab\". Should return: 2")
+  void lengthOfLongestSubstring6() {
+    String input = "aab";
+    int expected = 2;
+    int result = new Solution().lengthOfLongestSubstring(input);
+
+    assertEquals(expected, result);
+  }
+
+  @Test
+  @DisplayName("Input: \"dvdf\". Should return: 3")
+  void lengthOfLongestSubstring7() {
+    String input = "dvdf";
+    int expected = 3;
+    int result = new Solution().lengthOfLongestSubstring(input);
+
+    assertEquals(expected, result);
+  }
+
+  @Test
+  @DisplayName("Input: \"ckilbkd\". Should return: 5")
+  void lengthOfLongestSubstring8() {
+    String input = "ckilbkd";
+    int expected = 5;
+    int result = new Solution().lengthOfLongestSubstring(input);
+
+    assertEquals(expected, result);
+  }
+
+}
