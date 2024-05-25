f302bd78fb0d6f06557fd1ea65ca643a45d1aac7
Aidar Khabibullin <aydar.kh@gmail.com>
2024-05-25 18:04:24 +0500

    Add link to leetcode for problem #11

--- src/test/java/leetcode/problems/medium/p0011/SolutionTest.java
+++ src/test/java/leetcode/problems/medium/p0011/SolutionTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+// https://leetcode.com/problems/container-with-most-water/
 @DisplayName("Leetcode Problem #11. Container With Most Water")
 class SolutionTest {
 
