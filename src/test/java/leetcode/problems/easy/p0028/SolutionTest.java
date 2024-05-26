a6f9a30f952fc6e34abc797c99759ab04699440a
Aidar Khabibullin <aydar.kh@gmail.com>
2024-05-27 01:23:00 +0500

    Implement and test solution for LeetCode problem #28

---
+++ src/test/java/leetcode/problems/easy/p0028/SolutionTest.java
@@ -0,0 +1,23 @@
+package leetcode.problems.easy.p0028;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
+@DisplayName("LeetCode #28. Find the Index of the First Occurrence in a String")
+class SolutionTest {
+
+  @Test
+  @DisplayName("Input: haystack = \"sadbutsad\", needle = \"sad\". Output: 0")
+  void strStr1() {
+    assertEquals(0, new Solution().strStr("sadbutsad", "sad"));
+  }
+
+  @Test
+  @DisplayName("Input: haystack = \"leetcode\", needle = \"leeto\". Output: -1")
+  void strStr2() {
+    assertEquals(-1, new Solution().strStr("leetcode", "leeto"));
+  }
+}
