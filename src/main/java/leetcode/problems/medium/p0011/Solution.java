9b86116f9e0eb5339c39a7a8bf641dcc23b1400c
Aidar Khabibullin <aydar.kh@gmail.com>
2024-05-26 13:03:10 +0500

    Refactor variable names in Solution.java for clarity
    - Rename 'left' to 'leftIndex' and 'right' to 'rightIndex' for better
    readability

--- src/main/java/leetcode/problems/medium/p0011/Solution.java
+++ src/main/java/leetcode/problems/medium/p0011/Solution.java
@@ -3,22 +3,22 @@ package leetcode.problems.medium.p0011;
 public class Solution {
   public int maxArea(int[] height) {
     int max = 0;
-    int left = 0;
-    int right = height.length - 1;
+    int leftIndex = 0;
+    int rightIndex = height.length - 1;
 
     if (height.length == 2) {
       max = Math.min(height[0], height[1]);
     }
 
-    while (left < right) {
-      if (height[left] <= height[right]) {
-        max = Math.max(height[left] * (right - left), max);
-        left++;
-      } else if (height[left] >= height[right]) {
-        max = Math.max(height[right] * (right - left), max);
-        right--;
+    while (leftIndex < rightIndex) {
+      if (height[leftIndex] <= height[rightIndex]) {
+        max = Math.max(height[leftIndex] * (rightIndex - leftIndex), max);
+        leftIndex++;
+      } else if (height[leftIndex] >= height[rightIndex]) {
+        max = Math.max(height[rightIndex] * (rightIndex - leftIndex), max);
+        rightIndex--;
       } else {
-        max = Math.max(height[right] * (right - left), max);
+        max = Math.max(height[rightIndex] * (rightIndex - leftIndex), max);
         break;
       }
     }
