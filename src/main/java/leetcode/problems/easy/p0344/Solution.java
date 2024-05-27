f80e9d22ce67538a10b30292257e90f2bd98b228
Aidar Khabibullin <akf.mailbox@gmail.com>
2024-05-28 04:26:42 +0500

    Update Solution.java

--- src/main/java/leetcode/problems/easy/p0344/Solution.java
+++ src/main/java/leetcode/problems/easy/p0344/Solution.java
@@ -2,8 +2,7 @@ package leetcode.problems.easy.p0344;
 
 public class Solution {
   public void reverseString(char[] s) {
-    int len = s.length / 2;
-    for (int i = 0; i < len; i++) {
+    for (int i = 0; i < s.length / 2; i++) {
       char temp = s[i];
       s[i] = s[s.length - 1 - i];
       s[s.length - 1 - i] = temp;
