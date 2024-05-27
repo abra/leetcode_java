829fbfe436aeb748b2e335b250c16cd0506e5eb9
Aidar Khabibullin <aydar.kh@gmail.com>
2024-05-27 14:02:51 +0500

    Optimize romanToInt method

--- src/main/java/leetcode/problems/easy/p0013/Solution.java
+++ src/main/java/leetcode/problems/easy/p0013/Solution.java
@@ -4,37 +4,26 @@ import java.util.Map;
 
 public class Solution {
 
-  private static final Map<String, Integer> dict = Map.of(
-      "I", 1,
-      "V", 5,
-      "X", 10,
-      "L", 50,
-      "C", 100,
-      "D", 500,
-      "M", 1000
+  private static final Map<Character, Integer> dict = Map.of(
+      'I', 1,
+      'V', 5,
+      'X', 10,
+      'L', 50,
+      'C', 100,
+      'D', 500,
+      'M', 1000
   );
 
   public int romanToInt(String s) {
-
-    String[] l = s.split("");
-    int[] numbers = new int[l.length];
-
-    for (int i = 0; i < l.length; i++) {
-      numbers[i] = dict.get(l[i]);
-    }
-
     int sum = 0;
+    int n = s.length();
 
-    for (int j = numbers.length - 1; j >= 0; ) {
-      if (j - 1 >= 0 && numbers[j] > numbers[j - 1]) {
-        sum += numbers[j] - numbers[j - 1];
-        j -= 2;
-      } else if (j - 1 >= 0 && numbers[j] == numbers[j - 1]) {
-        sum += numbers[j] + numbers[j];
-        j -= 2;
+    for (int i = 0; i < n; i++) {
+      int current = dict.get(s.charAt(i));
+      if (i < n - 1 && current < dict.get(s.charAt(i + 1))) {
+        sum -= current;
       } else {
-        sum += numbers[j];
-        j--;
+        sum += current;
       }
     }
 
